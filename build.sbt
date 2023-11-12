7352f96a40f09caaa465284cee2feecd0a78fdf2
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-11-12 10:01:58 -0300

    feat: Implement Monad to ETree

--- build.sbt
+++ build.sbt
@@ -9,14 +9,15 @@ lazy val TrabalhoLIP = project
   .dependsOn(TrabalhoLIPCore)
   .settings(
     name := "TrabalhoLIP",
-    libraryDependencies += toolkitTest % Test,
-    libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.2.0"
+    libraryDependencies += "org.scala-lang" %% "toolkit-test" % "0.1.7" % Test,
+    libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.2.0",
+    libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.15",
+    libraryDependencies += "org.typelevel" %% "cats-core" % "2.10.0"
   )
 
 lazy val TrabalhoLIPCore = project
   .in(file("core"))
   .settings(
     name := "TrabalhoLIP Core",
-    libraryDependencies += "org.scala-lang" %% "toolkit" % "0.2.0",
-    libraryDependencies += toolkitTest % Test
+    libraryDependencies += "org.scala-lang" %% "toolkit-test" % "0.1.7" % Test
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,35 @@
-object Main {
-  import Parser._
-  import CoordBTree._
+import cats.{Applicative, Monad}
 
-  def main(args: Array[String]): Unit = {
-    println("Questão 1:")
-    CoordBTree.testCase()
+import scala.annotation.tailrec
 
-    println("Questão 2:")
-    Run.main(args)
+object Main extends App {
+  sealed trait ETree[*] extends Monad[ETree] {
+    override def pure[A](x: A): ETree[A] = Leaf(x)
+
+    override def flatMap[A, B](fa: ETree[A])(f: A => ETree[B]): ETree[B] =
+      fa match
+        case Leaf(a) => f(a)
+        case Branch(left, right) =>
+          Branch(
+            flatMap(left)(f),
+            flatMap(right)(f)
+          )
+
+    override def tailRecM[A, B](a: A)(f: A => ETree[Either[A, B]]): ETree[B] =
+      def toB(either: Either[A, B]): ETree[B] =
+        either match
+          case Left(x) =>
+            flatMap(f(x))(toB)
+          case Right(b) => Leaf(b)
+
+      flatMap(f(a))(toB)
+  }
+  case class Leaf[A](value: A)
+    extends ETree[A]
+  case class Branch[A](left: ETree[A], right: ETree[A])
+    extends ETree[A]
+
+  def main(): Unit = {
+    val mTree = Leaf[(String, Double)](("A", 0.66))
   }
 }
