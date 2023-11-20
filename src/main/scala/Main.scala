7c85e7d01937c1209faf93fbe0ba0d6642a0ea24
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-11-20 14:02:25 -0300

    feat: morse encode  algorithm

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,35 +1,61 @@
-import cats.{Applicative, Monad}
+import cats.{Applicative, Eval, Group, Monad, Monoid}
+import cats.syntax.all.*
 
 import scala.annotation.tailrec
+import scala.compiletime.ops.string.Length
 
-object Main extends App {
-  sealed trait ETree[*] extends Monad[ETree] {
-    override def pure[A](x: A): ETree[A] = Leaf(x)
+object Main {
+  trait Encode {
+    def format(num: Int): String = {
+      val ident = "\t".repeat(num)
+      this match
+        case Branch(totalFreq, left, right) =>
+          (s"$ident <total: ${totalFreq.toString.format("%.3f")}>"
+            + s"\n$ident ${left.format(num + 1)}"
+            + s"\n$ident ${right.format(num + 1)}")
+        case Letter(freq) => s"$ident <freq: ${freq.toString.format("%.3f")}>"
+    }
 
-    override def flatMap[A, B](fa: ETree[A])(f: A => ETree[B]): ETree[B] =
-      fa match
-        case Leaf(a) => f(a)
-        case Branch(left, right) =>
-          Branch(
-            flatMap(left)(f),
-            flatMap(right)(f)
+    def solve(list: List[Double]): Encode = {
+      list.sorted match
+        case head :: tail =>
+          tail.foldRight[Encode](this)(
+            (freq, code) => code.add(Letter(freq))
           )
+        case Nil => this
+    }
 
-    override def tailRecM[A, B](a: A)(f: A => ETree[Either[A, B]]): ETree[B] =
-      def toB(either: Either[A, B]): ETree[B] =
-        either match
-          case Left(x) =>
-            flatMap(f(x))(toB)
-          case Right(b) => Leaf(b)
+    def sum: Double = {
+      this match
+      case Letter(freq) => freq
+      case Branch(totalFreq, left, right) => totalFreq
+    }
 
-      flatMap(f(a))(toB)
+    def add(x: Letter): Branch = {
+      this match
+        case Letter(freq) =>
+          Branch(
+            freq + x.freq,
+            Letter(math.max(x.freq, freq)),
+            Letter(math.min(x.freq, freq))
+          )
+        case Branch(totalFreq, left, right) =>
+          val total = x.freq + totalFreq
+          if x.freq > right.sum
+            then Branch(total, x, Branch(totalFreq, left, right))
+            else Branch(total, left.add(x), right)
+    }
   }
-  case class Leaf[A](value: A)
-    extends ETree[A]
-  case class Branch[A](left: ETree[A], right: ETree[A])
-    extends ETree[A]
+  case class Letter(freq: Double)
+    extends Encode
+  case class Branch(totalFreq: Double, left: Encode, right: Encode)
+    extends Encode
 
-  def main(): Unit = {
-    val mTree = Leaf[(String, Double)](("A", 0.66))
+  def main(array: Array[String]): Unit = {
+    val end: Int = 100
+    val list = List[Double](20, 3, 4, 50, 5, 6).map(
+      i => 1f/i
+    )
+    println(Letter(.5).solve(list).format(0))
   }
 }
